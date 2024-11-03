bbfe38877c48c012e9a5a6dcac986a1de1cee618
LucasBlackLu <lucasblack997@gmail.com>
2024-11-03 23:29:42 +0800

    Revert "sm8350-common: Copy firmware to revovery"
    This reverts commit caf4f58e87a2fecec92b7da3c687021e9e61a356.

--- device.mk
+++ device.mk
@@ -11,11 +11,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FrameworksResT2q
 
-# Reovery firmware
-PRODUCT_COPY_FILES += \
-    vendor/samsung/t2q/proprietary/vendor/firmware/y792_t2.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/y792_t2.bin \
-    vendor/samsung/t2q/proprietary/vendor/firmware/y792_t2.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/y792_t2.bin
-
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/samsung/sm8350-common/common.mk)
 
